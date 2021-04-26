bc368a1b99e844b3ad57a20b55b5e8f74e37b10b
CheongFrancine <20020509@rp.edu.sg>
2021-04-26 13:17:00 +0800

    Some comment

--- app/src/main/java/sg/edu/rp/c346/id20020509/demomysecondapp/MainActivity3.java
+++ app/src/main/java/sg/edu/rp/c346/id20020509/demomysecondapp/MainActivity3.java
@@ -4,16 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import sg.edu.rp.c346.id20020509.demomysecondapp.R;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //This is a new line
-
+        setContentView(R.layout.activity_main3);
     }
 }
