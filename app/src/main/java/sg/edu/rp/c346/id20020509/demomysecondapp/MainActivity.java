a057fd990fde034991173a05b76546167f5b7dcd
CheongFrancine <20020509@rp.edu.sg>
2021-04-26 10:37:02 +0800

    Some comment

---
+++ app/src/main/java/sg/edu/rp/c346/id20020509/demomysecondapp/MainActivity.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.c346.id20020509.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sg.edu.rp.c346.id20020509.demomysecondapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //This is a new line
+
+        //new line
+
+        //
+
+    }
+}
